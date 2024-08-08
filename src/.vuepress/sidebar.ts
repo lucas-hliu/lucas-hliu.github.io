4acdd7b074e4d7168658f660551f6bb78f9e2b2b
liuheng <liuheng2015@gmail.com>
2024-08-08 15:06:32 +0800

    add hash map and refine

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -8,28 +8,33 @@ export default sidebar({
       icon: "laptop-code",
       prefix: "interview/",
       link: "interview/",
-      children: "structure",
+      children: [
+        "1 c++basic.md",
+        "2 process.md",
+        "3 thread.md"
+      ],
     },
     {
       text: "数据结构",
       icon: "book",
       prefix: "algo/",
       link: "algo/",
-      children: "structure",
+      children: [
+        "1 array_and_list.md",
+        "2 queue_and_stack.md",
+        "3 hash_map.md",
+      ],
     },
     {
       text: "架构设计",
       icon: "network-wired",
       prefix: "design/",
       link: "design/",
-      children: "structure",
-    },
-    {
-      text: "架构设计",
-      icon: "network-wired",
-      prefix: "design/",
-      link: "design/",
-      children: "structure",
+      children: [
+        "1 design_patterns.md",
+        "2 observer_pattern.md",
+        "3 strategy_pattern.md",
+      ],
     },
     {
       text: "书籍推荐",
--- src/README.md
+++ src/README.md
@@ -9,17 +9,20 @@ editLink: false
 
 本站是一个面向 c++ 程序员的知识库，主要包含一些基本的 c++ 面试题和数据结构算法。其目的在于帮助想学习 c++ 编程同学，掌握一些基本的算法和数据结构。主要有以下几部分：
 
-### c++ 相关的面/笔试题
+### 面试集锦
 
-- [c++面试题](/interview/c++basic)
-- [线程编程](/interview/thread)
-- [进程编程](/interview/process)
+- [c++面试题](/interview/1%20c++basic)
+- [线程编程](/interview/3%20thread)
+- [进程编程](/interview/2%20process)
 
-### 基本的数据结构
-- [数组与链表](/algo/array)
+### 数据结构
+- [数组与链表](/algo/1%20array_and_list)
+- [队列与栈](/algo/2%20queue_and_stack)
+- [哈希表](/algo/3%20hash_map)
   
 ### 架构设计
-- [设计原则](/design/design_patterns)  
-- [策略模式](/design/strategy_pattern)
+- [设计原则](/design/1%20design_patterns)  
+- [观察者模式](/design/2%observer_pattern)
+- [策略模式](/design/3%strategy_pattern)
 
  
